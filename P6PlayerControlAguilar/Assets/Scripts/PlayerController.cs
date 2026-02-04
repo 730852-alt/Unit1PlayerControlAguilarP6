ffafb3b23d97b4e137272a501ec8109a5ce5aef8
730852-alt <730852@student.dusd.net>
2026-02-04 13:49:59 -0800

    finish camera switching
    on the last bonus

--- P6PlayerControlAguilar/Assets/Scripts/PlayerController.cs
+++ P6PlayerControlAguilar/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,10 @@ public class PlayerController : MonoBehaviour
     private float turnSpeed = 50.0f;
     private float horizontalInput;
     private float forwardInput;
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,5 +24,11 @@ public class PlayerController : MonoBehaviour
         //move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+        if(Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
     }
 }
